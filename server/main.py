1926c7263e459514bc1f614961f82d483aaa3c8a
Alex Zimin <azimin@me.com>
2014-07-07 15:24:30 +0400

    - Fixes

--- server/main.py
+++ server/main.py
@@ -57,7 +57,16 @@ def add_entry():
     db.commit()
     flash("New entry was successfully posted")
     return redirect(url_for("show_entries"))
-        
+
+# @app.route("/remove", methods=["POST"])
+# def remove_entry():
+#     if not session.get("logged_in"):
+#         abort(401)
+#     db = get_db()
+#     db.session.delete('delete from entries where id=' + entry_id)
+#     db.commit()
+
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     error = None
