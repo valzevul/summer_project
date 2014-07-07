47c0e99411a0bdcf5b8cb2f2df2f8d6af9448e96
Alex Zimin <azimin@me.com>
2014-07-07 14:39:06 +0400

    - Server part update

--- server/main.py
+++ server/main.py
@@ -11,42 +11,89 @@ app.config.update(dict(
     DATABASE=os.path.join(app.root_path, "flaskr.db"),
     DEBUG=True,
     SECRET_KEY="development key",
-￼29
     USERNAME="admin",
-    PASSWORD="default
+    PASSWORD="default"
 ))
+app.config.from_envvar("FLASKR_SETTINGS", silent=True)
 
-#@app.route("/")
-#def hello():
-#    return "HiS"
+def connect_db():
+    rv = sqlite3.connect(app.config["DATABASE"])
+    rv.row_factory = sqlite3.Row
+    return rv
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return '''
-<html>
-  <head>
-  <style type="text/css">
-  body {
-    color: purple;
-    background-color: #e8e8e8 }
+def get_db():
+    if not hasattr(g, "sqlite_db"):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
 
-    button{
-    width: 100px;
-    height: 200px;
-    font-size: 200%;
-    background-color: #e8e8e8
-}
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, "sqlite_db"):
+        g.sqlite_db.close()
 
-  </style>
-    <title>Admin page</title>
-  </head>
-  <body>
-  <button type="button">+</button>
-  <button type="button">-</button>
-  </body>
-</html>
-'''
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource("schema.sql", mode="r") as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+        
+        
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    error = None
+    if request.method == "POST":
+        if request.form["username"] != app.config["USERNAME"]:
+            error = "Invalid username"
+        elif request.form["password"] != app.config["PASSWORD"]:
+            error = "Invalid password"
+        else:
+            session["logged_in"] = True
+            flash("You were logged in")
+            return redirect(url_for("show_entries"))
+    return render_template("login.html", error=error)
+
+@app.route("/logout")
+def logout():
+    session.pop("logged_in", None)
+    flash("You were logged out")
+    return redirect(url_for("show_entries"))
+
+# @app.route('/')
+# @app.route('/index')
+# def index():
+#     return '''
+# <html>
+#   <head>
+#   <style type="text/css">
+#   body {
+#     color: purple;
+#     background-color: #e8e8e8 }
+#
+#     button{
+#     width: 100px;
+#     height: 200px;
+#     font-size: 200%;
+#     background-color: #e8e8e8
+# }
+#
+#   </style>
+#     <title>Admin page</title>
+#   </head>
+#   <body>
+#   <button type="button">+</button>
+#   <button type="button">-</button>
+#   </body>
+# </html>
+# '''
+
+@app.route("/")
+def show_entries():
+    db = get_db()
+    cur = db.execute("select title, text from entries order by id desc")
+    entries = cur.fetchall()
+    return render_template("show_entries.html", entries=entries)
 
 if __name__ == "__main__":
     app.run( debug = True )
