65e5f8d1cf3cf8f196b5f89632e56dca0eb08282
Alex Zimin <azimin@me.com>
2014-07-07 14:58:00 +0400

    - Server DB

--- server/main.py
+++ server/main.py
@@ -38,7 +38,25 @@ def init_db():
         with app.open_resource("schema.sql", mode="r") as f:
             db.cursor().executescript(f.read())
         db.commit()
+
+@app.route("/")
+def show_entries():
+    db = get_db()
+    cur = db.execute("select title, text from entries order by id desc")
+    entries = cur.fetchall()
+    return render_template("show_entries.html", entries=entries)
         
+        
+@app.route("/add", methods=["POST"])
+def add_entry():
+    if not session.get("logged_in"):
+        abort(401)
+    db = get_db()
+    db.execute("insert into entries (title, text) values (?, ?)",
+                 [request.form["title"], request.form["text"]])
+    db.commit()
+    flash("New entry was successfully posted")
+    return redirect(url_for("show_entries"))
         
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -88,12 +106,6 @@ def logout():
 # </html>
 # '''
 
-@app.route("/")
-def show_entries():
-    db = get_db()
-    cur = db.execute("select title, text from entries order by id desc")
-    entries = cur.fetchall()
-    return render_template("show_entries.html", entries=entries)
 
 if __name__ == "__main__":
     app.run( debug = True )
